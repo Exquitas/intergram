53314fed889047f6bd07832a01d42dd6c3d7349b
Exquitas <31363553+Exquitas@users.noreply.github.com>
2021-12-15 11:53:37 +0100

    Update default-configuration.js

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -5,7 +5,7 @@ export const defaultConfiguration = {
     closedStyle: 'chat', // button or chat
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help you?',
+    introMessage: 'Hello! How can we help you? (Once you have started the chat, please do not leave or refresh the page or your session will get closed.)',
     autoResponse: 'Looking for the first available admin (It might take a minute). Please do not leave or refresh the page in the meantime.',
     autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
     'contact you, and we will get back to you as soon as we can.',
