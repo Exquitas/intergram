6d9462b1f0e4b27b08415414efa363a83e7b010a
Exquitas <31363553+Exquitas@users.noreply.github.com>
2022-01-05 16:33:01 +0100

    Update default-configuration.js

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -7,8 +7,8 @@ export const defaultConfiguration = {
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
     introMessage: 'Hello! How can we help you? (Once you have started the chat, please do not leave or refresh the page or your session will get closed.)',
     autoResponse: 'Looking for the first available admin (It might take a minute). Please do not leave or refresh the page in the meantime.',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
+    autoNoResponse: 'It seems that no one is available to answer right now. Please send us a message ' +
+    'to support@gunthy.zendesk.com and we will get back to you as soon as we can.',
     placeholderText: 'Send a message...',
     displayMessageTime: true,
     mainColor: '#1f8ceb',
